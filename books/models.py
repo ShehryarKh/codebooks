9f1fd26a243528b11a11ad9fa1f28ddf5edc8c26
Shehryar <shehryar2212@yahoo.com>
2016-12-25 15:03:20 -0500

    new branch

---
+++ books/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length= 120)
+    date_published = models.DateField()
+    buy_link = models.URLField(max_length=120)
+    images = models.ImageField(upload_to="photobook", blank=True,
+                                null= True)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    review = models.TextField()
+    avg_star = models.FloatField(validators = [MinValueValidator(0), MaxValueValidator(5)])
+    createdOn = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    number_of_books = models.IntegerField(max(1000))
---
+++ topbook/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from books import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'topbook.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', auth_views.login, name= 'login'),
+    url(r'^login/$', auth_views.logout, name= 'logout'),
+    url(r'^$', views.index, name='home')
+]
