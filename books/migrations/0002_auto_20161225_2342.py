6dba88d4ea44e66d83a40849284fa2015cdc0e38
Shehryar <shehryar2212@yahoo.com>
2016-12-25 20:24:25 -0500

    changed tables to add aurthor to book

---
+++ books/migrations/0002_auto_20161225_2342.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('books', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='book',
+            name='buy_link',
+            field=models.URLField(blank=True, max_length=120),
+        ),
+    ]
--- books/models.py
+++ books/models.py
@@ -6,10 +6,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    # number_of_books = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(1000)])
+
+
 class Book(models.Model):
+    author = models.ManyToManyField(Author)
     title = models.CharField(max_length= 120)
     date_published = models.DateField()
-    buy_link = models.URLField(max_length=120)
+    buy_link = models.URLField(max_length=120, blank=True)
     images = models.ImageField(upload_to="photobook", blank=True,
                                 null= True)
 
@@ -22,8 +29,3 @@ class Review(models.Model):
     createdOn = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User)
 
-class Author(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    number_of_books = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(1000)])
-    books = models.ManyToManyField(Book)
